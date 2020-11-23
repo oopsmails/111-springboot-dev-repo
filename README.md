deb65c28fad073aef8ca2865d9262116732c7e7b
oopsmails <oopsmails@gmail.com>
2020-11-22 21:19:16 -0500

    20201122: spring-boot-kafka-docker

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # springboot-dev-repo
 
+## spring-boot-kafka-docker
 
+
+## spring-boot-rest-sonarqube
 
---
+++ spring-boot-kafka-docker/src/test/java/com/oopsmails/springboot/kafka/SpringKafkaDockerIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsmails.springboot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaDockerIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
