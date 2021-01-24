eed848d99759ec10f7f68ca71003e3a79b358fc7
oopsmails <oopsmails@gmail.com>
2021-01-23 22:30:46 -0500

    20210123: moving in spring-boot-mock-backend

---
+++ spring-boot-mock-backend/src/test/java/com/oopsmails/springboot/mockbackend/employee/controller/EmployeeControllerTest.java
@@ -0,0 +1,86 @@
+package com.oopsmails.springboot.mockbackend.employee.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.oopsmails.springboot.mockbackend.SpringBootBackendMockApplication;
+import com.oopsmails.springboot.mockbackend.employee.model.Employee;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@SpringBootTest(classes = {
+        SpringBootBackendMockApplication.class,
+        EmployeeControllerTest.EmployeeControllerTestConfig.class
+})
+public class EmployeeControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+
+    @Test
+// @WithMockUser(username = "admin1", roles = "ADMIN")
+    public void getAllEmployeesAPI() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/employee-api")
+// .with(user("admin1").roles("ADMIN"))
+// .with(csrf())
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+// .andExpect(MockMvcResultMatchers.jsonPath("$.employees").exists())
+// .andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+        List<Employee> employeeList = objectMapper.readValue(content, typeFactory.constructCollectionType(List.class, Employee.class));
+
+        assertTrue(employeeList.size() > 0);
+    }
+
+    @TestConfiguration
+    public static class EmployeeControllerTestConfig {
+        @Bean
+        public Clock appClock() {
+            LocalDateTime mockNow = LocalDateTime.of(2019, Month.FEBRUARY, 20, 10, 11, 20);
+            Clock result = Clock.fixed(mockNow.atZone(ZoneId.of("Canada/Eastern")).toInstant(), ZoneId.of("Canada/Eastern"));
+
+            return result;
+        }
+    }
+}
+
