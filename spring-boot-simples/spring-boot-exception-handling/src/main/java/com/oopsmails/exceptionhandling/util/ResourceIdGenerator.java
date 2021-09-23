c3a713fbdcf8195633c401b42fed87d5671d59d7
oopsmails <oopsmails@gmail.com>
2021-09-23 11:48:05 -0400

    20210923: spring-boot-exception-handling

--- spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/util/ResourceIdGenerator.java
+++ spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/util/ResourceIdGenerator.java
@@ -5,7 +5,10 @@ import java.util.Random;
 public class ResourceIdGenerator {
 	
 	private static final Random random = new Random();
-	
+
+	private ResourceIdGenerator() {
+	}
+
 	public static Integer generateResourceId() {
 		// Id should be between 1000 and 9999 both inclusive
 		Integer randomResourceId = random.ints(1000, 10000).findFirst().getAsInt();
