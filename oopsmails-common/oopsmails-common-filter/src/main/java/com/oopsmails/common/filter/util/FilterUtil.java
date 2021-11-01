e7d858c0013f9755a4204cb698e5642f80d92763
oopsmails <oopsmails@gmail.com>
2021-11-01 16:06:21 -0400

    20211101: minor

--- oopsmails-common/oopsmails-common-filter/src/main/java/com/oopsmails/common/filter/util/FilterUtil.java
+++ oopsmails-common/oopsmails-common-filter/src/main/java/com/oopsmails/common/filter/util/FilterUtil.java
@@ -5,10 +5,14 @@ import java.util.Set;
 public class FilterUtil {
 
     public static boolean isExemptionUrl(final String path, Set<String> loggingExemptionUrls) {
-        for (String url : loggingExemptionUrls) {
-            if (path.indexOf(url) >= 0) {
-                return true;
-            }
+//        for (String url : loggingExemptionUrls) {
+//            if (path.indexOf(url) >= 0) {
+//                return true;
+//            }
+//        }
+
+        if (path != null && loggingExemptionUrls != null) {
+            return loggingExemptionUrls.stream().anyMatch(ele -> ele != null && path.trim().equals(ele.trim()));
         }
         return false;
     }
