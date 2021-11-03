e0923954e940a4623a7ebe5d9dc4d0fe0e4e953c
oopsmails <oopsmails@gmail.com>
2021-11-02 20:19:36 -0400

    20211102: url indexOf

--- oopsmails-common/oopsmails-common-filter/src/main/java/com/oopsmails/common/filter/util/FilterUtil.java
+++ oopsmails-common/oopsmails-common-filter/src/main/java/com/oopsmails/common/filter/util/FilterUtil.java
@@ -5,15 +5,15 @@ import java.util.Set;
 public class FilterUtil {
 
     public static boolean isExemptionUrl(final String path, Set<String> loggingExemptionUrls) {
-//        for (String url : loggingExemptionUrls) {
-//            if (path.indexOf(url) >= 0) {
-//                return true;
-//            }
-//        }
-
-        if (path != null && loggingExemptionUrls != null) {
-            return loggingExemptionUrls.stream().anyMatch(ele -> ele != null && path.trim().equals(ele.trim()));
+        for (String url : loggingExemptionUrls) {
+            if (path.indexOf(url) >= 0) {
+                return true;
+            }
         }
+
+//        if (path != null && loggingExemptionUrls != null) {
+//            return loggingExemptionUrls.stream().anyMatch(ele -> ele != null && path.trim().indexOf(ele.trim()) >= 0);
+//        }
         return false;
     }
 }
--- spring-boot-mock-backend/src/main/java/com/oopsmails/springboot/mockbackend/health/ServerHealthIndicator.java
+++ spring-boot-mock-backend/src/main/java/com/oopsmails/springboot/mockbackend/health/ServerHealthIndicator.java
@@ -1,10 +1,14 @@
 package com.oopsmails.springboot.mockbackend.health;
 
+import com.oopsmails.springboot.mockbackend.exception.OopsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
 @Component
@@ -13,6 +17,19 @@ public class ServerHealthIndicator implements HealthIndicator {
 
     private String ip;
     private LocalDateTime startDateTime;
+
+    @PostConstruct
+    public void postConstruct() {
+        startDateTime = LocalDateTime.now();
+
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            this.ip = inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("Failed to get host IP address, e: {}", e);
+            throw new OopsException("Failed to get host IP address, " + e.getMessage(), e);
+        }
+    }
 
     @Override
     public Health health() {
