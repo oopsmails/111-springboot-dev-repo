6b68d84a62f6232cfb4c894e9dae1508c1b0706c
oopsmails <oopsmails@gmail.com>
2021-11-11 14:44:52 -0500

    20211111: Scheduler using Mutex

--- spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/mutex/entity/InstantItem.java
+++ spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/mutex/entity/InstantItem.java
@@ -5,6 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.Instant;
 
+/**
+ * This class is used to handle select timestamp directly from db.
+ * Without this wrapper, will get error, "org.hibernate.MappingException: No Dialect mapping for JDBC type: 2014"
+ */
+
 @Entity
 public class InstantItem {
     private Instant date;
