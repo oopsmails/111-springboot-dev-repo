861d2b18764e7a692212a8a73da88ac972554e80
oopsmails <oopsmails@gmail.com>
2022-08-04 17:26:17 -0400

    20220804: compare json files

--- spring-boot-java-main/src/test/java/com/oopsmails/springboot/javamain/example/SpringBootExampleTest.java
+++ spring-boot-java-main/src/test/java/com/oopsmails/springboot/javamain/example/SpringBootExampleTest.java
@@ -4,6 +4,7 @@ import com.oopsmails.springboot.javamain.SpringBootJavaGenericTestBase;
 import com.oopsmails.springboot.javamain.model.Employee;
 import com.oopsmails.springboot.javamain.repository.EmployeeRepository;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,10 +15,12 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.util.JsonExpectationsHelper;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -71,11 +74,56 @@ public class SpringBootExampleTest extends SpringBootJavaGenericTestBase {
         System.out.println("prettyJsonStr = " + prettyJsonStr);
     }
     @Test
-    public void myTest() throws Exception {
-        String expectedJson = FileUtils.readFileToString(new File(getTestFileNameWithPath("./expected.json")));
-        String actualJson = FileUtils.readFileToString(new File(getTestFileNameWithPath("./actual-01.json")));
+    public void testJsonDiff1() throws Exception {
+//        String expectedJson = FileUtils.readFileToString(new File(getTestFileNameWithPath("./expected.json")));
+//        String actualJson = FileUtils.readFileToString(new File(getTestFileNameWithPath("./actual-01.json")));
 //        JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.STRICT);
-        JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+
+        String expectedJson = IOUtils.toString(new File(getTestFileNameWithPath("./expected.json")).toURI(), Charset.defaultCharset());
+        String actualJson = IOUtils.toString(new File(getTestFileNameWithPath("./actual-01.json")).toURI(), Charset.defaultCharset());
+//        JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+        JSONAssert.assertNotEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+    }
+    @Test
+    void testJsonDiff2() throws Exception {
+        final String json1 =
+                "{" +
+                        "  'someObject': {" +
+                        "    'someArray': [" +
+                        "      {" +
+                        "        'someInternalObject': {" +
+                        "          'value': '123'" +
+                        "        }" +
+                        "      }," +
+                        "      {" +
+                        "        'someInternalObject2': {" +
+                        "          'value': '456'" +
+                        "        }" +
+                        "      }" +
+                        "    ]" +
+                        "  }" +
+                        "}";
+
+        final String json2 =
+                "{" +
+                        "  'someObject': {" +
+                        "    'someArray': [" +
+                        "      {" +
+                        "        'someInternalObject': {" +
+                        "          'value': '123'" +
+                        "        }" +
+                        "      }," +
+                        "      {" +
+                        "        'someInternalObject2': {" +
+                        "          'value': '4567'" +
+                        "        }" +
+                        "      }" +
+                        "    ]" +
+                        "  }" +
+                        "}";
+
+//        new JsonExpectationsHelper().assertJsonEqual(json1, json2, true);
+        new JsonExpectationsHelper().assertJsonNotEqual(json1, json2, true);
     }
 
     @TestConfiguration
