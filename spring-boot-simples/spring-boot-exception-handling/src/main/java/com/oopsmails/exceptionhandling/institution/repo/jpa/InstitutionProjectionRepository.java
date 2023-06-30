06c82e38c2276ade82243c8e2b057ef1b7d6f9df
oopsmails <oopsmails@gmail.com>
2023-06-30 07:50:42 -0400

    20230630: More on Spring JPA, projection, out join

--- spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/institution/repo/jpa/InstitutionProjectionRepository.java
+++ spring-boot-simples/spring-boot-exception-handling/src/main/java/com/oopsmails/exceptionhandling/institution/repo/jpa/InstitutionProjectionRepository.java
@@ -11,11 +11,21 @@ import java.util.List;
 public interface InstitutionProjectionRepository extends JpaRepository<Institution, Long> {
 
     // Query to return branch IDs with Institution columns for all institutions
-    @Query("SELECT i.id AS institutionId, b.id AS branchId, i.institutionName AS institutionName, i.description AS description FROM Institution i JOIN i.branchList b")
+    @Query("SELECT i.id AS institutionId, " +
+            "b.id AS branchId, " +
+            "i.institutionName AS institutionName, " +
+            "i.description AS description " +
+//            "FROM Institution i JOIN i.branchList b")
+            "FROM Institution i LEFT JOIN i.branchList b ")
     List<BranchIdProjection> findAllBranchIdsWithInstitution();
 
     // Query to return full branches with Institution columns for a specific institution
-    @Query("SELECT i.id AS institutionId, b.id AS branchId, b.branchName AS branchName, i.institutionName AS institutionName, i.description AS description FROM Institution i JOIN i.branchList b WHERE i.id = :institutionId")
+    @Query("SELECT i.id AS institutionId, " +
+            "b.id AS branchId, " +
+            "b.branchName AS branchName, " +
+            "i.institutionName AS institutionName, " +
+            "i.description AS description " +
+            "FROM Institution i JOIN i.branchList b WHERE i.id = :institutionId")
     List<FullBranchProjection> findFullBranchesByInstitutionIdWithInstitution(@Param("institutionId") Long institutionId);
 
     // Projection for branch IDs with Institution columns
